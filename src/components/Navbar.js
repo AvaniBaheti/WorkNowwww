0763490dfbd00526a6eddd78a2b5f6c5b9575494
AvaniBaheti <76617903+AvaniBaheti@users.noreply.github.com>
2023-08-12 19:09:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,17 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Alert from 'react-bootstrap/Alert'
+
+import { DeleteContext } from './context/ContextProvider';
 export default function Navbar() {
+    const { dlttask, setDlettask } = useContext(DeleteContext);
     return (
+        <>
         <Box sx={{ flexGrow: 1 }}>
+
             <AppBar position="static" style={{ background: "#232f3e" }}>
+
                 <Toolbar>
                     <IconButton
                         size="large"
@@ -22,9 +29,17 @@ export default function Navbar() {
                     </IconButton>
                     <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} style={{ textAlign: "center" }}     >
                         WorkNow
+
                     </Typography>
+
                 </Toolbar>
             </AppBar>
         </Box>
+        {
+            dlttask ? <Alert variant="danger" onClose={() => setDlettask(false)} dismissible>
+                <Alert.Heading>Your Task remove Succesfully</Alert.Heading>
+            </Alert> : ""
+        }
+        </>
     )
 }
